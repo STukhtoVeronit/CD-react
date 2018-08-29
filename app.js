9981c3dc7a48457f88820c500a6116f793716144
Stanislau Tukhto <s.tukhto.vironit@vironit.ru>
2018-08-29 14:51:47 +0300

    Initialized base structure

---
+++ app.js
@@ -0,0 +1,7 @@
+let img = React.createElement('img', {src: 'img/react.png'});
+let tittle = React.createElement('h1', null, 'Hello boyz');
+let subtitle = React.createElement('p', null, 'today I gonna tell you a story');
+let container = React.createElement('div', {className: 'container'}, img, tittle, subtitle);
+
+ReactDOM.render(container, document.getElementById('root'));
+
